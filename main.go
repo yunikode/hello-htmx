8482d7c3e9b2fd2ed17b06d45fdbfdbe575f0e76
zee-german <franz.lutzenberger@gmail.com>
2023-12-16 11:28:28 +0100

    starting with go and htmx

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Film struct {
+	Title    string
+	Director string
+}
+
+func main() {
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		films := map[string][]Film{
+			"Films": {
+				{Title: "The Godfather", Director: "Francis For Coppola"},
+				{Title: "Blade Runner", Director: "Ridley Scott"},
+				{Title: "The Thing", Director: "John Carpenter"},
+			},
+		}
+		tmpl.Execute(w, films)
+	}
+
+	p1 := func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(1 * time.Second)
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
+	}
+	http.HandleFunc("/", h1)
+
+	http.HandleFunc("/add-film/", p1)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
